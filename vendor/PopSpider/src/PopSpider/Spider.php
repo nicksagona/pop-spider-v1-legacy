a7a88462faaae712a634cdc349993a0d9982f7ce
Nick Sagona <nick@moc10media.com>
2014-01-02 13:24:43 -0600

    Update copyright

--- vendor/PopSpider/src/PopSpider/Spider.php
+++ vendor/PopSpider/src/PopSpider/Spider.php
@@ -10,7 +10,7 @@ namespace PopSpider;
  * @category   PopSpider
  * @package    PopSpider
  * @author     Nick Sagona, III <nick@popphp.org>
- * @copyright  Copyright (c) 2009-2013 Moc 10 Media, LLC. (http://www.moc10media.com)
+ * @copyright  Copyright (c) 2009-2014 Moc 10 Media, LLC. (http://www.moc10media.com)
  * @license    https://github.com/nicksagona/PopSpider/blob/master/LICENSE.TXT     New BSD License
  * @version    1.1.1
  */
